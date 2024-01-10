2354532a9cda8a6cf89807f4c9ac5b333b06b3b6
gootibi <gootibi@gmail.com>
2024-01-11 00:47:31 +0100

    25 + 5 Clock project. Changes to be committed: 	new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.js 	new file:   public/vite.svg 	new file:   src/App.tsx 	new file:   src/assets/Alarm.mp3 	new file:   src/assets/react.svg 	new file:   src/components/Display.tsx 	new file:   src/components/TimeSetter.tsx 	new file:   src/components/helpers.ts 	new file:   src/index.css 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   tailwind.config.js 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/App.tsx
@@ -0,0 +1,133 @@
+import { useEffect, useState } from 'react';
+import { DisplayState } from './components/helpers';
+import TimeSetter from './components/TimeSetter';
+import Display from './components/Display';
+import Alarm from './assets/Alarm.mp3'
+
+const defaultBreakTime: number = 5 * 60;
+const defaultSessionTime: number = 25 * 60;
+const min: number = 60;
+const max: number = 60 * 60;
+const interval: number = 60;
+
+
+function App() {
+  const [breakTime, setBreakTime] = useState<number>(defaultBreakTime);
+  const [sessionTime, setSessionTime] = useState<number>(defaultSessionTime);
+  const [displayState, setDisplayState] = useState<DisplayState>({
+    time: sessionTime,
+    timeType: "Session",
+    timerRunning: false,
+  });
+
+  useEffect(() => {
+    let timerID: number;
+    if (!displayState.timerRunning) {
+      return;
+    };
+
+    if (displayState.timerRunning) {
+      timerID = window.setInterval(decrementDisplay, 1000);
+    }
+
+    return () => {
+      window.clearInterval(timerID);
+    };
+  }, [displayState.timerRunning]);
+
+  useEffect(() => {
+    if (displayState.time === 0) {
+      const audio = document.getElementById("beep") as HTMLAudioElement;
+      audio.currentTime = 5;
+      audio.play().catch((error) => console.log(error));
+      setDisplayState((prev) => ({
+        ...prev,
+        time: prev.timeType === "Session" ? breakTime : sessionTime,
+        timeType: prev.timeType === "Session" ? "Break" : "Session",
+      }));
+    }
+  }, [displayState, breakTime, sessionTime]);
+
+  const reset = () => {
+    setBreakTime(defaultBreakTime);
+    setSessionTime(defaultSessionTime);
+    setDisplayState({
+      time: defaultSessionTime,
+      timeType: "Session",
+      timerRunning: false,
+    });
+    const audio = document.getElementById("beep") as HTMLAudioElement;
+    audio.pause();
+    audio.currentTime = 8;
+  };
+
+  const startStop = () => {
+    setDisplayState((prev) => ({
+      ...prev,
+      timerRunning: !prev.timerRunning,
+    }));
+  };
+
+  const changeBreakTime = (time: number) => {
+    if (displayState.timerRunning) {
+      return;
+    };
+    setBreakTime(time);
+  };
+
+  const decrementDisplay = () => {
+    setDisplayState((prev) => ({
+      ...prev,
+      time: prev.time - 1,
+    }));
+  };
+
+  const changeSessionTime = (time: number) => {
+    if (displayState.timerRunning) {
+      return;
+    };
+    setSessionTime(time);
+    setDisplayState({
+      time: time,
+      timeType: "Session",
+      timerRunning: false,
+    });
+  };
+
+  return (
+    <div className='app'>
+      <div className="settings">
+        <div className="break">
+          <h4 id="break-label">Break Length</h4>
+          <TimeSetter
+            time={breakTime}
+            setTime={changeBreakTime}
+            min={min}
+            max={max}
+            interval={interval}
+            type="break"
+          />
+        </div>
+        <div className="session">
+          <h4 id="session-label">Session Length</h4>
+          <TimeSetter
+            time={sessionTime}
+            setTime={changeSessionTime}
+            min={min}
+            max={max}
+            interval={interval}
+            type="session"
+          />
+        </div>
+      </div>
+      <Display
+        displayState={displayState}
+        reset={reset}
+        startStop={startStop}
+      />
+      <audio id="beep" src={Alarm}><track kind="captions"/></audio>
+    </div>
+  );
+};
+
+export default App;
